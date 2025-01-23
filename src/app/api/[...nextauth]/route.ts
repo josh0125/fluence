bb8afd2ce2947c50e100e439a9d50d669cda9eaf
Joshua Allen <joshuatallen99@gmail.com>
2025-01-23 15:58:02 -0700

    Deleted uneeded folder

--- src/app/api/[...nextauth]/route.ts
+++
@@ -1,6 +0,0 @@
-import NextAuth from "next-auth";
-import { authOptions } from "./authOptions";
-
-// The handler for the route should use `NextAuth`
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST }; // Export both GET and POST handlers
